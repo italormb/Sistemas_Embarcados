98d41d532aefc8e12ea15fc59762162f22c11bcd
italormb <italrmb@gmail.com>
2018-04-22 19:46:48 -0300

    q1 e q1 do ex6.2

---
+++ Atividades/EX_6/q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]){
+    int i=0;
+    int a[10];
+
+    
+
+    while(argv[i] != NULL){
+    	a[i]=system(argv[i+1]);
+    		i++;
+    }
+    
+    return 0;
+}
