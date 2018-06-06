0dae10fa467398131e1c1ceaf7d2f5775c4cb687
italormb <italrmb@gmail.com>
2018-06-06 09:38:05 -0300

    update

---
+++ photo_gray.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.misc import imread
+a=imread("placa.png")
+#a.shape
+#plt.imshow(a, cmap= "gray")
+#plt.imshow(a[:,:,0])
+#plt.show()
+#plt.imshow(a[:,:,1])
+#plt.show()
+#plt.imshow(a[:,:,1], cmap= "gray")
+#plt.show()
+#b=a[:,:,1]
+#plt.imshow(b, cmap= "gray")
+#plt.show()
+#b.shape
+#c=b.reshape([b.shape[0]*b.shape[1]])
+#c.shape
+#plt.imshow(c)
+
+
+
+
+
+
